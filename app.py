8bed53d2befb0b43dfee9cb3ced1ed7ec32c6623
Gouda_Clark <mclarkuga@gmail.com>
2020-11-24 17:09:25 -0500

    added rhens nest scraping

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def search():
 
 @app.route('/results/<term>')
 def results(term):
+    # CLUBHOUSE INFO
     source = requests.get('https://www.clubhousekidandcraft.com/search?q={}'.format(term)).text
     soup = BeautifulSoup(source, "html.parser")
     soup.prettify(formatter='html')
@@ -25,26 +26,53 @@ def results(term):
     linx = []
     alts = []
     sites= []
-    
     for t in thumbnails:
         imgs = t.findAll("img")
-        for image in imgs:
-            n = str(image).split(" src=\"")
-            if len(n) > 1:
-                link = n[1][:-9]
-                linx.append(link)
-                a = str(image).split("alt=\"")
-                alt = str(a).split("\"")[0][11:]
-                alts.append(alt)
-        anchors = t.findAll("a")
-        for anchor in anchors:
-            i = str(anchor).split("<a href=\"")
-            if len(i) > 1:
-                j = str(i[1]).split("\" title=")[0]
-                sites.append(j) 
+        if len(imgs) > 0:
+            for image in imgs:
+                n = str(image).split(" src=\"")
+                if len(n) > 1:
+                    link = n[1][:-9]
+                    linx.append(link)
+                    a = str(image).split("alt=\"")
+                    alt = str(a).split("\"")[0][11:]
+                    alts.append(alt)
+            anchors = t.findAll("a")
+            for anchor in anchors:
+                i = str(anchor).split("<a href=\"")
+                if len(i) > 1:
+                    j = str(i[1]).split("\" title=")[0]
+                    sites.append(j) 
+    # RHEN'S NEST INFO
+    rnsource = requests.get('https://rhensnesttoyshop.com/search?type=product&options%5Bprefix%5D=last&q={}'.format(term)).text
+    rnsoup = BeautifulSoup(rnsource, "html.parser")
+    rnsoup.prettify(formatter='html')
+    rnthumbnails = rnsoup.findAll("div", {"class":"rimage-wrapper"})
+    rnlinx = []
+    rnalts = []
+    rnsites = []
+    for t in rnthumbnails:
+        imgs = t.findAll("img")
+        if len(imgs)> 0:
+            for image in imgs:
+                n = str(image).split("data-src=\"")
+                if len(n)>1:
+                    link = n[1].split("{width}x.jpg")[0]
+                    rnlinx.append(link)
+                    # print(rnlinx)
+    rntitles = rnsoup.findAll("div", {"class":"product-block__title"})
+    for title in rntitles:
+        if len(title)>0:
+            x = str(title).split("ss=r\">")[1]
+            rnalt = str(x).split("</a>")[0]
+            rnalts.append(rnalt)
+
+    # print('RHENS NEST')
+    # print(rnsource)
     Html_file = open("raw.html", "w")
-    Html_file.write(str(thumbnails))
+    Html_file.write(str(rnthumbnails))
     Html_file.close()
     return render_template('results.html', term=term, thumbnails=thumbnails, 
-            image=image, link=link, linx=linx, alts=alts, sites = sites
+            linx=linx, alts=alts, sites=sites,
+            rnlinx=rnlinx, rnalts=rnalts
             )
